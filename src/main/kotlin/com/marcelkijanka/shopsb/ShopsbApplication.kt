5e631c51031b9d954bb560132c735cd6772e6e7e
Marcel Kijanka <30501750+marcelkijanka@users.noreply.github.com>
2020-11-01 12:48:26 +0100

    init

---
+++ src/main/kotlin/com/marcelkijanka/shopsb/ShopsbApplication.kt
@@ -0,0 +1,11 @@
+package com.marcelkijanka.shopsb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShopsbApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShopsbApplication>(*args)
+}
---
+++ src/test/kotlin/com/marcelkijanka/shopsb/ShopsbApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.marcelkijanka.shopsb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShopsbApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
