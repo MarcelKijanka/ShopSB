06bd4c721400c06e16fb8c8fa38df9cc9994d401
marcelkijanka <marcel.mck5@gmail.com>
2020-11-15 16:36:28 +0100

    style(link): Move /api to properties instead of Controller declaration

--- src/main/kotlin/com/marcelkijanka/shopsb/controllers/ItemsController.kt
+++ src/main/kotlin/com/marcelkijanka/shopsb/controllers/ItemsController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
-@RequestMapping("api/items")
+@RequestMapping("items")
 class ItemsController(private val itemsService: ItemsService) {
 
     @GetMapping
